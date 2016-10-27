ed57288eb9196723c4736a02e7d98e3752cd8464
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-10-27 17:31:50 +0300

    Fixing problem: dpkg-preconfigure: unable to re-open stdin: No such file or directory

--- sandbox/bootstrap.sh
+++ sandbox/bootstrap.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
+sudo dpkg-reconfigure debconf -f noninteractive -p critical
+
+# Update packages:
 apt-get update
+
+# Apache install:
 apt-get install -y apache2
+#apt-get install -y apache2 > null 2>&1
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+
