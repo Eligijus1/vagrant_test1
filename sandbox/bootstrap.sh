f833465b602741fd6f3a1bef8da98e72835f72e6
Eligijus Stugys <eligijus.stugys@nfq.lt>
2016-11-22 15:51:34 +0200

    Installing PHP 7 and MySQL

--- sandbox/bootstrap.sh
+++ sandbox/bootstrap.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+echo "# IPv4 and IPv6 localhost aliases:" | sudo tee /etc/hosts
+echo "127.0.0.1 vagrant_test1.domainname.com  vagrant_test1  localhost" | sudo tee -a /etc/hosts
+echo "::1       vagrant_test1.domainname.com  vagrant_test1  localhost" | sudo tee -a /etc/hosts
+echo "10.0.2.15 vagrant_test1.domainname.com  vagrant_test1  localhost" | sudo tee -a /etc/hosts
+
 sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
 sudo dpkg-reconfigure debconf -f noninteractive -p critical
 
+# Fixing languages:
+sudo apt-get install -y language-pack-en-base
+sudo LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php
+
 # Update packages:
 apt-get update
+
+# Install nmap:
+sudo apt-get install -y nmap
+
+# Install MySQL:
+echo "mysql-server-5.5 mysql-server/root_password password root" | debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password root" | debconf-set-selections
+sudo apt-get -y install mysql-server-5.5
 
 # Apache install:
 apt-get install -y apache2
@@ -13,4 +30,8 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+
+# Installing PHP 7:
+sudo apt-get install -y php
+
 
