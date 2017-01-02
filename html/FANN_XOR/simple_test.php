41f235259530a499ca3b9c479241b2b136c56e83
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-01-02 10:38:46 +0200

    Added FANN XOR to example some notes 2016.12.30

--- html/FANN_XOR/simple_test.php
+++ html/FANN_XOR/simple_test.php
@@ -1,19 +1,33 @@
 <?php
+// Define application start:
 $milliseconds = round(microtime(true) * 1000);
 
+// Define train file location:
 $train_file = (dirname(__FILE__) . "/xor_float.net");
+
+// Interrupt application if train file not exist:
 if (!is_file($train_file))
     die("The file xor_float.net has not been created! Please run simple_train.php to generate it");
 
+// Constructs a backpropagation neural network from a configuration file:
 $ann = fann_create_from_file($train_file);
+
+// Check if neural network created:
 if (!$ann)
     die("ANN could not be created");
 
+// Array of input values:
 $input = array(-1, 1);
+
+// Will run input through the neural network, returning an array of outputs, 
+// the number of which being equal to the number of neurons in the output layer:
 $calc_out = fann_run($ann, $input);
 printf("INFO: xor test (%f,%f) -> %f\n", $input[0], $input[1], $calc_out[0]);
 echo "<br>";
+
+// Destroys the entire network and properly freeing all the associated memory:
 fann_destroy($ann);
 
+// Inform about application finish and print time used:
 echo "INFO: Done simple_test.php in " . (round(microtime(true) * 1000) - $milliseconds) ." milliseconds<br>";
 
--- html/FANN_XOR/simple_train.php
+++ html/FANN_XOR/simple_train.php
@@ -1,23 +1,50 @@
 <?php
+// Define application start time:
 $milliseconds = round(microtime(true) * 1000);
 
-$num_input = 2;
-$num_output = 1;
+// The total number of layers including the input and the output layer:
 $num_layers = 3;
+
+// Number of neurons in the first layer:
+$num_input = 2;
+
+// Number of neurons in the second layer:
 $num_neurons_hidden = 3;
+
+// Number of neurons in the 3rd (Third) layer - output:
+$num_output = 1;
+
 $desired_error = 0.001;
+
+// The maximum number of epochs the training should continue:
 $max_epochs = 500000;
+
+// The number of epochs between calling a user function. 
+// A value of zero means that user function is not called.
 $epochs_between_reports = 1000;
 
+/* Creates a standard fully connected backpropagation neural network
+ * There will be a bias neuron in each layer (except the output layer), 
+ * and this bias neuron will be connected to all neurons in the next layer. 
+ * When running the network, the bias nodes always emits 1.
+ */
 $ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
 
+// Check if returns a neural network resource:
 if ($ann) {
+    // Sets the activation function for all of the hidden layers (FANN_SIGMOID_SYMMETRIC - Symmetric sigmoid activation function, aka. tanh.):
     fann_set_activation_function_hidden($ann, FANN_SIGMOID_SYMMETRIC);
+    
+    // Sets the activation function for the output layer:
     fann_set_activation_function_output($ann, FANN_SIGMOID_SYMMETRIC);
 
+    // Define the file containing train data:
     $filename = dirname(__FILE__) . "/xor.data";
+    
+    // Trains on an entire dataset, which is read from file, for a period of time:
     if (fann_train_on_file($ann, $filename, $max_epochs, $epochs_between_reports, $desired_error))
     {
+        // Saves the entire network to a configuration file:
         fann_save($ann, dirname(__FILE__) . "/xor_float.net");
         echo "INFO: saved to file " . dirname(__FILE__) . "/xor_float.net<br>";
     }
@@ -26,6 +53,7 @@ if ($ann) {
         echo "ERROR: fann_train_on_file returned false.<br>";
     }
 
+    // Destroys the entire network and properly freeing all the associated memory:
     fann_destroy($ann);
 }
 else
@@ -33,5 +61,6 @@ else
     echo "ERROR: ann is empty.<br>";
 }
 
+// Inform about application finish and print time used:
 echo "INFO: Done simple_train.php in " . (round(microtime(true) * 1000) - $milliseconds) ." milliseconds<br>";
 
