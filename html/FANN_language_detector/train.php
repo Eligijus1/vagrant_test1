67c93e5d51b2060df2fcfe3a2df694f1412343b8
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-01-06 10:03:29 +0200

    Finished initial train version 2017.01.06

--- html/FANN_language_detector/train.php
+++ html/FANN_language_detector/train.php
@@ -7,48 +7,54 @@
 // Define application start time:
 $milliseconds = round(microtime(true) * 1000);
 
-/**
- * Define network parameters.
- * 256 - input parameters amount. It should be equal to letter in alphabet amount.
- * 128 - neurons in the second layer amount. NOTE: define this number by experiments.
- * 3   - input signals amount. We will su support 3 signals - 3 languages.
- * 1.0 - connection rate. If the connection rate is set to 1, the network will be fully connected,
- *       but if it is set to 0.5 only half of the connections will be set. Better not change it.
- * 0.7 - learning rate. The learning rate is used to determine how aggressive training should be for some of
- *       the training algorithms
- */
-$ann = fann_create(array(256, 128, 3), 1.0, 0.7);
-/*
-fann_train($ann,
-    array(
-        array(
-            generate_frequencies(file_get_contents("en.txt")), // Inputs
-            array(1, 0, 0) // Outputs
-        ),
-        array(
-            generate_frequencies(file_get_contents("fr.txt")), // Inputs
-            array(0, 1, 0) // Outputs
-        ),
-        array(
-            generate_frequencies(file_get_contents("pl.txt")), // Inputs
-            array(0, 0, 1) // Outputs
-        ),
-    ),
-    100000,
-    0.00001,
-    1000
-);
-*/
-/**
- * Saves the entire network to a configuration file.
- */
-// fann_save($ann, "classify.txt");
+// The total number of layers including the input and the output layer:
+$num_layers = 3;
 
-// Destroys the entire network and properly freeing all the associated memory:
-fann_destroy($ann);
+// Number of neurons in the first layer. We will su support 256 signals - it should be equal to alphabet amount:
+$num_input = 256;
+
+// Number of neurons in the second layer. NOTE: Define this number by experiments.:
+$num_neurons_hidden = 128;
+
+// Number of neurons in the 3rd (Third) layer - output. We have 3, because supporting 3 languages:
+$num_output = 3;
+
+// Creates a standard fully connected backpropagation neural network:
+$ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
+
+// Check if returns a neural network resource:
+if ($ann) {
+    fann_train($ann,
+        array(
+            array(
+                generate_frequencies(file_get_contents("en.txt")), // Inputs
+                array(1, 0, 0) // Outputs
+            ),
+            array(
+                generate_frequencies(file_get_contents("fr.txt")), // Inputs
+                array(0, 1, 0) // Outputs
+            ),
+            array(
+                generate_frequencies(file_get_contents("pl.txt")), // Inputs
+                array(0, 0, 1) // Outputs
+            ),
+        ),
+        100000,
+        0.00001,
+        1000
+    );
+
+    // Saves the entire network to a configuration file:
+    fann_save($ann, "classify.txt");
+
+    // Destroys the entire network and properly freeing all the associated memory:
+    fann_destroy($ann);
+} else {
+    echo 'ERROR: ann is empty.<br>';
+}
 
 // Inform about application finish and print time used:
-echo "INFO: Done train.php in " . (round(microtime(true) * 1000) - $milliseconds) ." milliseconds<br>";
+echo 'INFO: Done train.php in ' . (round(microtime(true) * 1000) - $milliseconds) . ' milliseconds<br>';
 
 /**
  * Method responsible to calculate letters using frequency.
@@ -57,7 +63,6 @@ echo "INFO: Done train.php in " . (round(microtime(true) * 1000) - $milliseconds
  *
  * @return array
  */
-/*
 function generate_frequencies($text)
 {
     // Removed all, live just letters:
@@ -74,4 +79,3 @@ function generate_frequencies($text)
 
     return array_values($data);
 }
-*/
