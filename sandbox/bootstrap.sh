569123cd786f07cbf9e2c6a67ce640a5e63a479b
Eligijus Stugys <eligijus.stugys@nfq.lt>
2017-03-20 17:19:16 +0200

    Fixed not required code.

--- sandbox/bootstrap.sh
+++ sandbox/bootstrap.sh
@@ -132,10 +132,23 @@ php /usr/local/bin/symfony demo
 sudo chown -R www-data:vagrant symfony_demo
 sudo chmod -R 775 symfony_demo
 
-# Install MailHog:
+# Install docker:
 sudo apt install -y docker.io
+
+# Install GO:
 sudo apt install -y golang-go
 export GOPATH=~/go
+echo "" | tee -a ~/.bashrc
+echo "# GO envarment variables:" | tee -a ~/.bashrc
+echo "export GOPATH=~/go" | tee -a ~/.bashrc
+echo "export PATH=\$PATH:$GOPATH/bin" | tee -a ~/.bashrc
+# echo "" | tee -a /home/vagrant/.bashrc
+# echo "# GO envarment variables:" | tee -a /home/vagrant/.bashrc
+# echo "export GOPATH=~/go" | tee -a /home/vagrant/.bashrc
+# echo "export PATH=\$PATH:$GOPATH/bin" | tee -a /home/vagrant/.bashrc
+
+# Install MailHog:
+echo "------------------------------ BEGIN MailHog install -----------"
 cd ~/
 wget https://github.com/mailhog/MailHog/releases/download/v0.2.1/MailHog_linux_amd64
 sudo mv MailHog_linux_amd64 /usr/bin/mailhog
@@ -148,10 +161,6 @@ echo "sendmail_path = /usr/bin/mhsendmail" | sudo tee -a /etc/php/7.1/apache2/ph
 sudo service apache2 stop
 sudo service apache2 start
 docker run -d -p 1025:1025 -p 8025:8025 mailhog/mailhog
-echo "" | tee -a ~/.bashrc
-echo "# GO envarment variables:" | tee -a ~/.bashrc
-echo "export GOPATH=~/go" | tee -a ~/.bashrc
-echo "export PATH=\$PATH:$GOPATH/bin" | tee -a ~/.bashrc
 sudo tee /etc/init/mailhog.conf <<EOL
 description "Mailhog"
 start on runlevel [2345]
@@ -161,9 +170,63 @@ pre-start script
     docker run -d -p 1025:1025 -p 8025:8025 mailhog/mailhog 1>/dev/null 2>&1
 end script
 EOL
+echo "------------------------------ BEGIN MailHog install -----------"
 
 # Install Sylius:
-# composer create-project -s beta sylius/sylius-standard sylius
-
-
+# echo "------------------------------ BEGIN Sylius install ------------"
+# cd /home/vagrant/
+# composer create-project --no-interaction -s beta sylius/sylius-standard sylius
+# sudo chown -R www-data:vagrant sylius
+# sudo chmod -R 775 sylius
+# mysql -u root --port=3306 -proot -t <<EOF
+# SHOW DATABASES;
+# CREATE DATABASE sylius;
+# SHOW DATABASES;
+# EOF
+# mysqladmin -u root --port=3306 -proot reload
+# echo "<VirtualHost *:80>
+#      ServerAdmin eligijus.stugys@gmail.com
+#      ServerName sylius.vagrant.test1.dev
+#      ServerAlias www.sylius.vagrant.test1.dev
+# 
+#      DocumentRoot /home/vagrant/sylius/web
+#      <Directory /home/vagrant/sylius/web>
+#           AllowOverride All
+#           Require all granted
+#      </Directory>
+# 
+#      ErrorLog /home/vagrant/sylius/logs/error.log
+#      CustomLog /home/vagrant/sylius/logs/access.log combined
+# </VirtualHost>
+# " | sudo tee /etc/apache2/sites-available/sylius.vagrant.test1.dev.conf
+# sudo a2ensite sylius.vagrant.test1.dev.conf
+# echo "date.timezone = Europe/Vilnius" | sudo tee -a /etc/php/7.1/cli/php.ini
+# echo "date.timezone = Europe/Vilnius" | sudo tee -a /etc/php/7.1/apache2/php.ini
+# sudo service apache2 stop
+# sudo service apache2 start
+# echo "parameters:
+#     database_driver: pdo_mysql
+#     database_host: 127.0.0.1
+#     database_port: 3306
+#     database_name: sylius
+#     database_user: root
+#     database_password: root
+#     mailer_transport: smtp
+#     mailer_host: 127.0.0.1
+#     mailer_user: null
+#     mailer_password: null
+#     secret: EDITME
+#     locale: en_US
+# " | tee /home/vagrant/sylius/app/config/parameters.yml
+# cd /home/vagrant/sylius/
+# npm install
+# npm run gulp
+# php bin/console sylius:install:check-requirements
+# cd /home/vagrant/sylius/
+# php bin/console sylius:install -n
+# cd /home/vagrant/
+# sudo chown -R www-data:vagrant sylius
+# sudo chmod -R 775 sylius
+# http://sylius.vagrant.test1.dev/app_dev.php
+# echo "------------------------------ END Sylius install --------------"
 
